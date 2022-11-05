30a31b9615f97fd2e2ebdc198c4cb97782aa20fc
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-05 16:48:47 -0700

    added screenshot example

---
+++ examples/take_screenshot.py
@@ -0,0 +1,21 @@
+import asyncio
+
+import ciberedev
+
+# creating our client instance, and passing a token
+client = ciberedev.Client(token="...")
+
+
+async def main():
+    # starting our client with a context manager
+    async with client:
+        # taking our screenshot
+        screnshot = await client.take_screenshot("www.google.com")
+        # printing the screenshots url
+        print(screnshot.url)
+
+
+# checking if this file is the one that was run
+if __name__ == "__main__":
+    # if so, run the main function
+    asyncio.run(main())
