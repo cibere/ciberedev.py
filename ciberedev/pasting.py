21610217e81cd5ee8bd8d8a17a8bac7338ae8086
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-05 13:59:30 -0700

    Update pasting.py

--- ciberedev/pasting.py
+++ ciberedev/pasting.py
@@ -1,6 +1,7 @@
-import aiohttp
-from typing import TypedDict
-from .errors import InvalidSessionError
+from typing import Optional, TypedDict
+
+from aiohttp import ClientSession
+
 
 class RawPasteData(TypedDict):
     url: str
@@ -8,30 +9,38 @@ class RawPasteData(TypedDict):
     status_code: int
     message: str
 
+
 class Paste:
     def __init__(self, *, data: RawPasteData):
         self.url = data.get("url")
         self.code = data.get("code")
 
-async def create_paste(text: str, *, session: aiohttp.ClientSession = None) -> Paste:
+
+
+async def create_paste(text: str, *, session: Optional[ClientSession] = None) -> Paste:
     """Creates a paste
-    
+
     :text: the text you want sent to the paste
-    
     :session: if you already have an aiohttp session that you would like to be used, you can pass it here
     """
+    close_client = False
+    if not session:
+        session = ClientSession()
+        close_client = True
     
-    async def _create_paste(session_to_use: aiohttp.ClientSession):
-        res = await session_to_use.post("https://paste.cibere.dev/upload", data={'text' : text}, verify_ssl=False)
-        raw_data = await res.json()
-        paste = Paste(data=raw_data)
+    try:
+        data = {"text": text}
+
+        request = await session.post(
+            "https://paste.cibere.dev/upload", data=data, verify_ssl=False
+        )
+        json = await request.json()
+        paste = Paste(data=json)
         return paste
     
-    if session:
-        try:
-            return await _create_paste(session)
-        except:
-            raise InvalidSessionError()
-    else:
-        async with aiohttp.ClientSession() as cs:
-            return await _create_paste(cs)
+    except Exception as e:
+        raise e
+    
+    finally:
+        if close_client:
+            await session.close()
