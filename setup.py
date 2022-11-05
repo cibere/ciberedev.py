17a60656604d884f6c0905af8de91f2a22794722
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-05 16:10:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,20 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="ciberedev.py",
-    version=ciberedev.__version__,
     author="cibere",
     author_email="cibere.dev@gmail.com",
+    url="https://github.com/cibere/cibere-dev.py",
+    project_urls={
+        "Code": "https://github.com/cibere/ciberedev.py",
+        "Issue tracker": "https://github.com/cibere/ciberedev.py/issues",
+        "Discord/Support Server": "https://discord.gg/2MRrJvP42N",
+    },
+    version=ciberedev.__version__,
+    python_requires=">=3.8",
+    install_requires=REQUIREMENTS,
     packages=["ciberedev"],
     description=ciberedev.__description__,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/cibere/cibere-dev.py",
     license="MIT",
-    python_requires=">=3.8",
-    install_requires=REQUIREMENTS,
 )
