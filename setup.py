9e14a618a8faa76ffd402d4dfec9a6bc1f4bd9a1
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-05 15:40:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import ciberedev
 
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
-  
+
 with open("requirements.txt", "r") as f:
     REQUIREMENTS = f.read().splitlines()
 
 setuptools.setup(
-    name="ciberedev",
+    name="ciberedev.py",
     version=ciberedev.__version__,
     author="cibere",
     author_email="cibere.dev@gmail.com",
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/cibere/cibere-dev.py",
-    license='MIT',
-    python_requires='>=3.8',
-    install_requires=REQUIREMENTS
+    license="MIT",
+    python_requires=">=3.8",
+    install_requires=REQUIREMENTS,
 )
