de78acf3f0a621b10799bee8f1592f91e9914da4
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-06 10:03:29 -0800

    updated description

--- ciberedev/__init__.py
+++ ciberedev/__init__.py
@@ -1,10 +1,10 @@
 """
 cibere.dev python wrapper
 ~~~~~~~~~~~~~~~~~~~
-A basic wrapper cibere.dev
+A basic wrapper cibere.dev (and discord oauth2)
 """
 
-__description__ = "A basic wrapper cibere.dev"
+__description__ = "A basic wrapper cibere.dev (and discord oauth2)"
 __version__ = "0.1.0"
 
 from .client import Client
