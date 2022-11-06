0155da957dd9e42ebdd835bcd3c93a817e457029
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-05 20:41:57 -0700

    added support for discord oauth user endpoitn

--- ciberedev/discord_oauth.py
+++ ciberedev/discord_oauth.py
@@ -31,6 +31,24 @@ class PartialGuild:
         self.features: list[str] = raw_data["features"]
 
 
+class PartialUser:
+    def __init__(self, *, raw_data: dict):
+        self.id: int = int(raw_data["id"])
+        self.name: str = raw_data["username"]
+        self.discriminator: int = int(raw_data["discriminator"])
+
+        self.bot: bool = bool(raw_data.get("bot", False))
+        self.system: bool = bool(raw_data.get("system", False))
+        self.mfa_enabled: bool = bool(raw_data.get("mfa_enabled", False))
+
+    @property
+    def username(self):
+        return self.name
+
+    def __str__(self):
+        return f"{self.name}#{self.discriminator}"
+
+
 class DiscordOauth:
     _session: ClientSession
     _authorization: Authorization
@@ -57,10 +75,11 @@ class DiscordOauth:
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
         res = await self._session.post(
-            self._authorization.discord_oauth.api_endpoint + "oauth2/token", data=data, headers=headers  # type: ignore
+            self._authorization.discord_oauth.api_endpoint + "/oauth2/token", data=data, headers=headers  # type: ignore
         )
         try:
             token = await res.json()
+            print(f"PAYLOAD RECIEVED: {token}")
             token = token["access_token"]  # type: ignore
         except KeyError:
             raise InvalidTokenGiven(token)
@@ -85,10 +104,11 @@ class DiscordOauth:
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
         res = await self._session.post(
-            self._authorization.discord_oauth.api_endpoint + "oauth2/token", data=data, headers=headers  # type: ignore
+            self._authorization.discord_oauth.api_endpoint + "/oauth2/token", data=data, headers=headers  # type: ignore
         )
         try:
             token = await res.json()
+            print(f"PAYLOAD RECIEVED: {token}")
             token = token["access_token"]
         except KeyError:
             raise InvalidCodeGiven(code)
@@ -105,6 +125,7 @@ class DiscordOauth:
             headers={"Authorization": f"Bearer {token}"},
         )
         data = await res.json()
+        print(f"PAYLOAD RECIEVED: {data}")
         cons = []
         for con in data:
             cons.append(Connections(raw_data=con))
@@ -116,11 +137,25 @@ class DiscordOauth:
         :token: the token you got from exchanging the oauth2 code
         """
         res = await self._session.get(
-            "https://discord.com/api/v10/users/@me/connections",
+            "https://discord.com/api/v10/users/@me/guilds",
             headers={"Authorization": f"Bearer {token}"},
         )
         data = await res.json()
+        print(f"PAYLOAD RECIEVED: {data}")
         guilds = []
         for guild in data:
             guilds.append(PartialGuild(raw_data=guild))
         return guilds
+
+    async def get_user_info(self, token: str) -> PartialUser:
+        """Get a users info
+
+        :token: the token you got from exchanging the oauth2 code
+        """
+        res = await self._session.get(
+            "https://discord.com/api/v10/users/@me",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+        data = await res.json()
+        user = PartialUser(raw_data=data)
+        return user
