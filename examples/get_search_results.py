9b36903d699aa932c237a3f68da6f23b6056fb4d
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-13 07:49:36 -0800

    added example for search endpoint

---
+++ examples/get_search_results.py
@@ -0,0 +1,24 @@
+import asyncio
+
+import ciberedev
+
+# creating our client instance
+client = ciberedev.Client()
+
+
+async def main():
+    # starting our client with a context manager
+    async with client:
+        # getting our search results
+        results = await client.search("cibere.dev")
+
+        # printing the first results title, url, and description
+        print(results[0].title)
+        print(results[0].url)
+        print(results[0].description)
+
+
+# checking if this file is the one that was run
+if __name__ == "__main__":
+    # if so, run the main function
+    asyncio.run(main())
