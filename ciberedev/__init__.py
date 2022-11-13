5b6f5f4048a3f572225eab7e428111ffa0e96b21
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-12 18:02:41 -0800

    added authorization stuff to the __init__ for easy importing

--- ciberedev/__init__.py
+++ ciberedev/__init__.py
@@ -7,4 +7,5 @@ A basic wrapper cibere.dev (and discord oauth2)
 __description__ = "A basic wrapper cibere.dev (and discord oauth2)"
 __version__ = "0.2.0"
 
+from .authorization import Authorization, FileUploaderAuthorization
 from .client import Client
