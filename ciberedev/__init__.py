e5ead9e6a2ccfe7e612405c8838f67c4551c7682
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-13 07:55:12 -0800

    updasted description

--- ciberedev/__init__.py
+++ ciberedev/__init__.py
@@ -1,10 +1,10 @@
 """
 cibere.dev python wrapper
 ~~~~~~~~~~~~~~~~~~~
-A basic wrapper cibere.dev (and discord oauth2)
+A basic wrapper cibere.dev
 """
 
-__description__ = "A basic wrapper cibere.dev (and discord oauth2)"
+__description__ = "A basic wrapper cibere.dev"
 __version__ = "0.2.0"
 
 from .authorization import Authorization, FileUploaderAuthorization
