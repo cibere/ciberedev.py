1434fbf9e9793d5db75dbd46b471e1e6e9c2cbc5
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-13 08:12:52 -0800

    Update __init__.py

--- ciberedev/__init__.py
+++ ciberedev/__init__.py
@@ -9,30 +9,3 @@ __version__ = "0.2.1"
 
 from .authorization import Authorization, FileUploaderAuthorization
 from .client import Client
-
-
-def _get_version():
-    VERSION = __version__
-    if VERSION.startswith("BETA"):
-        try:
-            import subprocess
-
-            p = subprocess.Popen(
-                ["git", "rev-list", "--count", "HEAD"],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            out, err = p.communicate()
-            if out:
-                VERSION += out.decode("utf-8").strip()
-            p = subprocess.Popen(
-                ["git", "rev-parse", "--short", "HEAD"],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            out, err = p.communicate()
-            if out:
-                VERSION += "+g" + out.decode("utf-8").strip()
-        except Exception:
-            pass
-    return VERSION
