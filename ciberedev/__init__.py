0b2b4d383d631134eaadf9a842247e04a824a6fa
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-27 12:49:03 -0800

    Update __init__.py

--- ciberedev/__init__.py
+++ ciberedev/__init__.py
@@ -7,5 +7,4 @@ A basic wrapper cibere.dev
 __description__ = "A basic wrapper cibere.dev"
 __version__ = "0.3.0"
 
-from .authorization import Authorization, FileUploaderAuthorization
 from .client import Client
