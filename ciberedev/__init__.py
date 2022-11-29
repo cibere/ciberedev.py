25e674e3872a5dc558de1e177eda45a726662973
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-28 19:47:32 -0800

    updated version and imports in __init__

--- ciberedev/__init__.py
+++ ciberedev/__init__.py
@@ -5,6 +5,30 @@ A basic wrapper cibere.dev
 """
 
 __description__ = "A basic wrapper cibere.dev"
-__version__ = "0.3.1"
+__version__ = "0.3.1b"
+
+from typing import Literal, NamedTuple
 
 from .client import Client
+from .errors import *
+from .screenshot import Screenshot
+from .searching import SearchResult
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: str
+
+
+raw_version = __version__.split(".")
+readable_releaselevel = {"a": "alpha", "b": "beta", "c": "candidate"}
+version_info = VersionInfo(
+    major=int(raw_version[0]),
+    minor=int(raw_version[1]),
+    micro=int(raw_version[2][0]),
+    releaselevel=readable_releaselevel.get(raw_version[2][1], "final"),
+)
+
+del Literal, NamedTuple, VersionInfo, readable_releaselevel, raw_version
