1e712c4c1e3c393d48fedc496a55fdee282c78ad
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-29 21:09:38 -0800

    fixed error with version parsing

--- ciberedev/__init__.py
+++ ciberedev/__init__.py
@@ -23,12 +23,17 @@ class VersionInfo(NamedTuple):
 
 
 raw_version = __version__.split(".")
-readable_releaselevel = {"a": "alpha", "b": "beta", "c": "candidate"}
+try:
+    raw_release_releaselevel = {"a": "alpha", "b": "beta", "c": "candidate"}.get(
+        raw_version[2][1], "final"
+    )
+except IndexError:
+    raw_release_releaselevel = "final"
 version_info = VersionInfo(
     major=int(raw_version[0]),
     minor=int(raw_version[1]),
     micro=int(raw_version[2][0]),
-    releaselevel=readable_releaselevel.get(raw_version[2][1], "final"),
+    releaselevel=raw_release_releaselevel,
 )
 
-del Literal, NamedTuple, VersionInfo, readable_releaselevel, raw_version
+del Literal, NamedTuple, VersionInfo, raw_release_releaselevel, raw_version
