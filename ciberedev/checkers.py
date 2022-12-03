f232152e395a7a2cd0b31f1048ec281aa00cd57f
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-12-03 11:12:20 -0800

    finished queen piece + added docstrings

--- ciberedev/checkers.py
+++ ciberedev/checkers.py
@@ -1,37 +1,49 @@
 from __future__ import annotations
 
-from io import BytesIO
 from typing import TYPE_CHECKING, Literal, Optional, Union
 
 from typing_extensions import Self
+
+from .errors import LocationAlreadyTaken, UnableToDemote, UnableToPromote
+from .file import File
 
 if TYPE_CHECKING:
     from .http import HTTPClient
 
 
 class PlayingPiece:
+    _raw_conversion = {"black": "b", "red": "r"}
+
     def __init__(
         self,
         *,
-        _type: Literal["black", "red", "black queen", "red queen"],
+        _type: Literal["black", "red"],
         location: int,
         board: CheckersGame,
     ):
+        """Creates a playing piece object
+
+        Parameters
+        ----------
+        _type: `str`
+            The type of piece it should be. `black`/`red`
+        location: `int`
+            where it should be put on the board
+        board: `ciberedev.checkers.CheckersGame`
+            the board it should be apart of
+        """
+
         self.type = _type
-        self.raw = {"black": "b", "red": "r", "black queen": "q", "red queen": "k"}[
-            self.type
-        ]
+        self.raw = self._raw_conversion[self.type]
         self._location = location
         self._board = board
 
-    def __str__(self):
-        return self.type
-
     def __repr__(self):
-        return f"<PlayingPiece type='{self.type}' location: '{self._location}'"
+        return f"<{self.__class__.__name__} type='{self.type}' location: '{self._location}'"
 
     @property
     def location(self) -> int:
+        """Returns the location on the board the piece is at"""
         return self._location
 
     @location.setter
@@ -42,14 +54,67 @@ class PlayingPiece:
         self.move(new_location)
 
     def move(self, new_location: int, /):
+        """Moves the piece to the given location
+
+        Parameters
+        ----------
+        new_location: `int`
+            the location the piece should be moved to
+
+        Raises
+        ----------
+        TypeError
+            Invalid location given
+        LocationAlreadyTaken
+            The location already has a piece there
+        """
+
         if new_location < 0 or new_location > 32:
             raise TypeError("The location must be inbetween 0 and 32")
         if not isinstance(self._board.pieces[new_location], EmptySpace):
-            raise TypeError("There is already a piece at the given location")
+            raise LocationAlreadyTaken(new_location)
 
         self._board.pieces[self._location] = EmptySpace()
         self._board.pieces[new_location] = self
         self._location = new_location
+
+    def promote(self) -> None:
+        """Promotes the piece to a queen
+
+        Raises
+        ----------
+        UnableToPromote
+            The piece is already a queen, and thus can not be promoted anymore
+        """
+
+        if isinstance(self, QueenPiece):
+            raise UnableToPromote()
+
+        new_piece = QueenPiece(
+            _type=self.type, location=self._location, board=self._board  # type: ignore
+        )
+        self._board.pieces[self._location] = new_piece
+
+    def demote(self) -> None:
+        """Demotes a piece from a queen
+
+        Raises
+        ----------
+        UnableToDemote
+            This piece is already a normal piece, and thus can not be demoted anymore
+        """
+
+        if isinstance(self, PlayingPiece):
+            raise UnableToDemote()
+
+        new_piece = PlayingPiece(
+            _type=self.type, location=self._location, board=self._board
+        )
+        self._board.pieces[self._location] = new_piece
+
+
+class QueenPiece(PlayingPiece):
+    _raw_conversion = {"black": "q", "red": "k"}
 
 
 class EmptySpace:
@@ -60,7 +125,22 @@ class CheckersGame:
     __slots__ = ["_http", "pieces"]
 
     def __init__(self, *, http_client: HTTPClient):
-        self.pieces: list[Union[PlayingPiece, EmptySpace]] = []
+        """Starts a checkers game
+
+        It is not recommended to start this yourself
+
+        Parameters
+        ----------
+        http_client: `ciberedev.http.HTTPClient`
+            The HTTPClient
+
+        Attributes
+        ----------
+        pieces: list[Union[`PlayingPiece`, `EmptySpace`, `QueenPiece`]]
+            A list of all the pieces in order
+        """
+
+        self.pieces: list[Union[PlayingPiece, EmptySpace, QueenPiece]] = []
         self._http = http_client
 
         for i in range(8):
@@ -70,7 +150,21 @@ class CheckersGame:
         for i in range(8):
             self.pieces.append(PlayingPiece(_type="red", location=i, board=self))
 
-    async def get_piece_at(self, index: int) -> Optional[PlayingPiece]:
+    async def get_piece_at(
+        self, index: int
+    ) -> Optional[Union[PlayingPiece, QueenPiece]]:
+        """Gets a piece at the given index
+
+        Parameters
+        ----------
+        index: `int`
+            The index to get the piece at
+
+        Returns
+        ----------
+        Optional[Union[`PlayingPiece`, `QueenPiece`]]
+        """
+
         piece = self.pieces[index]
         if isinstance(piece, EmptySpace):
             return None
@@ -79,6 +173,22 @@ class CheckersGame:
 
     @classmethod
     def from_pattern(cls, *, pattern: str, http_client: HTTPClient) -> Self:
+        """Starts a chess game from a given pattern
+
+        It is not recommended to start this yourself
+
+        Parameters
+        ----------
+        pattern: str
+            The pattern to set the board at
+        http_client: `ciberedev.http.HTTPClient`
+            The HTTPClient
+
+        Attributes
+        ----------
+        pieces: list[Union[`PlayingPiece`, `EmptySpace`, `QueenPiece`]]
+            A list of all the pieces in order
+        """
         if len(pattern) != 32:
             raise TypeError("Pattern must be 32 characters long")
         if not all(char in ["b", "_", "r", "q", "k"] for char in pattern):
@@ -110,8 +220,24 @@ class CheckersGame:
         return self
 
     async def to_pattern(self) -> str:
+        """Returns the pattern the current board is at
+
+        Returns
+        ----------
+        str
+        """
+
         return "".join(piece.raw for piece in self.pieces)
 
-    async def generate_board(self) -> bytes:
+    async def generate_board(self) -> File:
+        """Generates the boards image
+
+        Returns
+        ----------
+        ciberedev.file.File
+        """
+
         pattern = await self.to_pattern()
-        return await self._http.get_checkers_board(pattern)
+        raw = await self._http.get_checkers_board(pattern)
+        file = File(raw_bytes=raw)
+        return file
