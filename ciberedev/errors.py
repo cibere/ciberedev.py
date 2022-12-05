e1f74a63e9b3e18f7a861d3b2b954b2f425a0b74
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-12-04 20:41:06 -0800

    added UnknownDataReturned, APIException, UnknownStatusCode

--- ciberedev/errors.py
+++ ciberedev/errors.py
@@ -8,14 +8,13 @@ __all__ = [
     "UnableToDemote",
     "UnableToPromote",
     "LocationAlreadyTaken",
+    "UnknownDataReturned",
+    "APIException",
+    "UnknownStatusCode",
 ]
 
 
 class CiberedevException(Exception):
-    pass
-
-
-class APIException(CiberedevException):
     pass
 
 
@@ -29,6 +28,64 @@ class BoardgameException(CiberedevException):
 
 class CheckersException(BoardgameException):
     pass
+
+
+class APIException(CiberedevException):
+    def __init__(self, error: str):
+        """Creates an APIException error stinace
+
+        This is raised when the api returns an error
+        It is not recommended to raise this yourself
+
+        """
+
+        super().__init__(error)
+
+
+class UnknownStatusCode(APIException):
+    def __init__(self, code: int):
+        """Creates an UnknownStatusCode error stinace
+
+        This is raised when the api returns an unknown status code
+        It is not recommended to raise this yourself
+
+        Parameters
+        ----------
+        code: `int`
+            the status code that was returned
+
+        Attributes
+        ----------
+        code: `int`
+            the status code that was returned
+        """
+
+        self.code = code
+        super().__init__(f"API returned an unknown status code: '{self.code}'")
+
+
+class UnknownDataReturned(APIException):
+    def __init__(self, endpoint: str):
+        """Creates an UnknownDataReturned error stinace
+
+        This is raised when the data the api returns does not match what the client believes it should return
+        It is not recommended to raise this yourself
+
+        Parameters
+        ----------
+        endpoint: `str`
+            The endpoint the client is making a request to when this happend
+
+        Attributes
+        ----------
+        endpoint: `str`
+            The endpoint the client is making a request to when this happend
+        """
+
+        self.endpoint = endpoint
+        super().__init__(
+            f"API returned unknown data when making a request to '{endpoint}'"
+        )
 
 
 class APIOffline(APIException):
