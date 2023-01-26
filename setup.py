653648274d353682c8f84f41f5f6c7b5c2ff636c
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-01-25 17:17:22 -0800

    Update setup.py
    - added classifiers
    - added docs to project_urls
    - updated email

--- setup.py
+++ setup.py
@@ -13,9 +13,10 @@ packages = ["ciberedev", "ciberedev.types"]
 setuptools.setup(
     name="ciberedev.py",
     author="cibere",
-    author_email="cibere.dev@gmail.com",
+    author_email="contact@cibere.dev",
     url="https://github.com/cibere/ciberedev.py",
     project_urls={
+        "Documentation": "https://docs.cibere.dev/stable/index.html",
         "Code": "https://github.com/cibere/ciberedev.py",
         "Issue tracker": "https://github.com/cibere/ciberedev.py/issues",
         "Discord/Support Server": "https://discord.gg/2MRrJvP42N",
@@ -28,4 +29,20 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
 )
